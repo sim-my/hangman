d3d0e0d2efea82fc9455c9d2e1077f4510726571
Simmy <45229088+sim-my@users.noreply.github.com>
2019-08-03 18:15:58 +0545

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -2,32 +2,41 @@
 
 import random
 
+#random list of the word
 words = ["secret","various","water","life","death"]
 
+#this selects the random word from the given word list
 given = random.choice(words)
 
 print('*'*50)
 print('WELCOME TO HANGMAN')
 print('*'*50)
 print('RULES:')
-print('Guess the letter of the word for 8 times.')
-print('You gotta make correct guess of word in 8 times')
+print('Guess the letter of the word')
+print('You can make wrong guess for 8 times')
 print('IF... guess is correct, CONGRATULATION, YOU WIN')
 print('ELSE..., Sorry, YOU LOSE')
 print('*'*50)
 
-#initialize the number of the guess
+#initialize the number of turns for the guess
 turns = 8
 
-#start the loop till the guess count ends
+#creating the output for the hangman after each guess
 output = []
 output.extend(given)
 for i in range(0,len(given)):
     output[i] = "*"
 print('Word: '," ".join(output))
+
+#loop till the number of turns end
 while turns != 0:
+    #enter the guess each time till the turn is given
     guess = input("Enter your guess: ") 
-        
+
+#check if the guess is correct
+#if the guess is correct turn couter doesn't decremented
+#if the guess is wrong the turn counter is decremented
+
     if guess in given:
         print('Well done captain!!, It\'s a correct guess.')
         print('You\'ve',turns,'turns remaining, now!!')
@@ -36,7 +45,7 @@ while turns != 0:
         turns = turns - 1
         print('Don\'t worry still,you\'ve got', turns, 'turns left.')
             
-        
+#check if guess is correct display the right guessed letter        
     for j in range(0,len(given)):
         if guess == given[j]:
             output[j] = guess
@@ -44,11 +53,14 @@ while turns != 0:
         
     print('Word: ',' '.join(output))
     print('*'*50)
-        
+
+  #if you guess the word before the turn ends, you can get out of the loop 
+
     if output == list(given):
         break
               
-
+ #print the result of the game
+   
 if output == list(given):
     print('CONGRATULATION, YOU WIN!!!')
 else:
